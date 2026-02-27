467102ce47b18218cd541ef2300b1455896aaeba
Chris Morris <morris27sky@gmail.com>
2026-02-27 21:50:53 +0000

    Track MySQL init scripts so shard databases auto-create on new machines

---
+++ docker/mysql/init/01-create-dbs.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS vetmybuilder_test_s1_4_w0;
+CREATE DATABASE IF NOT EXISTS vetmybuilder_test_s1_4_w1;
+CREATE DATABASE IF NOT EXISTS vetmybuilder_test_s1_4_w2;
+CREATE DATABASE IF NOT EXISTS vetmybuilder_test_s1_4_w3;
